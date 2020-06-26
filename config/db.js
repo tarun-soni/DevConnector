b54badf243873816bafc3967a549ee137aeb5442
Kanchan Soni <soni_kan@India.XoriantCorp.com>
2020-06-26 22:57:41 +0530

    added more routes

--- config/db.js
+++ config/db.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
+//config will get values stored in config/default.json
 const config = require('config');
 
 const db = config.get('mongoURI')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true
-        });
-
+        await mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
         console.log('MongoDB connected');
     } catch (err) {
         console.log('error is ', err.message);
--- server.js
+++ server.js
@@ -3,12 +3,22 @@ const app = express();
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000;
 
+//method from db.js
 connectDB();
 
 
 app.get('/', (req, res) =>
     res.send('API Running')
 )
+
+// Define Routes
+//if user goes to api/SOMETHING we will redirect to routes/api/SOMETHING
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
+
 app.listen(PORT, () =>
     console.log(`Server listening on port ${PORT}`)
 )
