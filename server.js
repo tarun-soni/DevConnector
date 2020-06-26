b642358c117143ae86e8bbf32e75946b73bbe8d6
Kanchan Soni <soni_kan@India.XoriantCorp.com>
2020-06-26 22:20:33 +0530

    mongoDB connected

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
-
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000;
+
+connectDB();
+
 
 app.get('/', (req, res) =>
     res.send('API Running')
 )
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+app.listen(PORT, () =>
+    console.log(`Server listening on port ${PORT}`)
+)
