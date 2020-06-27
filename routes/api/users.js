2e441b8430209537881fa9222fbde42c52036f4a
Kanchan Soni <soni_kan@India.XoriantCorp.com>
2020-06-27 18:59:36 +0530

    added post method for user validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-//@route       GET api/users
+//@route       POST api/users
 //@desc        test route
 //@access      public
-router.get('/', (req, res) =>
-    res.send('User route')
-);
+const nameErrorMsg = " Name is Required'"
+const emailErrorMsg = " Please input a valid email address"
+const passwordErrorMsg = "Please enter a pass with 6 or more characters"
+
+router.post('/', [
+    check('name', nameErrorMsg).not().isEmpty(),
+    check('email', emailErrorMsg).isEmail(),
+    check('password', passwordErrorMsg).isLength({ min: 6 })
+],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        console.log('res body>>>>', res.body());
+        res.send('user route');
+    });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 //method from db.js
 connectDB();
 
+//Init middleware
+app.use(express.json({ extended: false }))
+
 
 app.get('/', (req, res) =>
     res.send('API Running')
