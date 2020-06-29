4bbd63bbe58e4fc96188197f15706c0e966d9c10
Tarun Soni <33228915+tarun-soni@users.noreply.github.com>
2020-06-29 23:08:55 +0530

    gets user data(json) using token
    x-auth-token and append the token , it will give info of user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth')
+
+const User = require('../../models/User');
+
 //@route       GET api/auth
 //@desc        test route
 //@access      public
-router.get('/', auth, (req, res) =>
-    res.send('Auth route')
+router.get('/', auth, async (req, res) => {
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+
+    } catch (err) {
+        console.error('error in auth route >>>>', err)
+        res.status(500).json({ msg: 'Server Error' });
+    }
+}
 );
 
 module.exports = router;
