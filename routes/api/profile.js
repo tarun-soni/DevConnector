ebf80f078cccc149013fb01a58775003282e3f7e
Tarun Soni <33228915+tarun-soni@users.noreply.github.com>
2020-06-30 15:44:00 +0530

    profile route
    hit /profile/me with x-auth-token

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
+//@route       GET api/profile/me
+//@desc        get current users profile
+//@access      private
+router.get('/me', auth, async (req, res) => {
+    try {
 
-//@route       GET api/profile
-//@desc        test route
-//@access      public
-router.get('/', (req, res) =>
-    res.send('Profile route')
+        const profile = await Profile.findOne({ user: req.user.id }).populate(
+            'user', ['name', 'avatar']
+        );
+
+        if (!profile) {
+            return res.status(400).json({ msg: ' There is No Profile for this user' })
+        }
+
+        res.json(profile)
+    } catch (err) {
+        console.error('error in Profile route >>>>', err)
+        res.status(500).json({ msg: 'Server Error' });
+    }
+}
 );
 
 module.exports = router;
