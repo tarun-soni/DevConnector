71cf10ea0ef446b5aed378979eac641fa5db573b
Tarun Soni <33228915+tarun-soni@users.noreply.github.com>
2020-07-09 22:47:11 +0530

    added Nav and Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
 import './App.css';
+import Landing from './components/layouts/Landing';
+import Navbar from './components/layouts/Navbar';
 
 const App = () => (
   <Fragment>
-    <h1>Hello </h1>
+    <Navbar />
+    <Landing />
   </Fragment>
 )
 export default App;
