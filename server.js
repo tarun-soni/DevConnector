48d4cbedb7afabe6ca073428100685b5532e75ac
Tarun Soni <33228915+tarun-soni@users.noreply.github.com>
2020-07-24 13:51:51 +0530

    prepare to host

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000;
-
+const path = require('path')
 //method from db.js
 connectDB();
 
@@ -10,9 +10,9 @@ connectDB();
 app.use(express.json({ extended: false }))
 
 
-app.get('/', (req, res) =>
-    res.send('API Running')
-)
+// app.get('/', (req, res) =>
+//     res.send('API Running')
+// )
 
 // Define Routes
 //if user goes to api/SOMETHING we will redirect to routes/api/SOMETHING
@@ -20,6 +20,17 @@ app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 app.listen(PORT, () =>
